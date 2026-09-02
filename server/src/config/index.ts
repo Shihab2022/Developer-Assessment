9a453d8994ca68e76edba06c08e32680806bd62c
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2026-09-02 22:57:37 +0600

    create some basic folder and file

---
+++ server/src/config/index.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join((process.cwd(), ".env")) });
+
+export default {
+  NODE_ENV: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET!,
+  reset_access_secret: process.env.RESET_PASS_ACCESS_SECRET!,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET!,
+  jwt_access_expire_in: process.env.JWT_ACCESS_EXPIRES_IN,
+  reset_pass_access_expire_in: process.env.RESET_PASS_ACCESS_EXPIRES_IN,
+  jwt_refresh_expire_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  reset_pass_base_link: process.env.RESET_PASS_BASE_LINK,
+  front_end_base_url: process.env.FRONT_END_BASE_URL,
+};
